7977b6471965a0ca66efb5b783a674c54eb3d528
Artem Smirnov <artemsmirnov90@gmail.com>
2012-09-29 22:38:10 +0400

    test

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * User: artegz
+ * Date: 29.09.12
+ * Time: 22:37
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world.");
+    }
+
+}
